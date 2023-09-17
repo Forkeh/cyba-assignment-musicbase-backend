7ff70f8337bed0d2c8a6123895e5b905547c1fcc
AliHMohammad <alihmohammad@hotmail.com>
2023-09-17 20:59:05 +0200

    getting undefined on return. Need fixing

--- utils/utils.js
+++ utils/utils.js
@@ -57,7 +57,7 @@ async function getTracksIDByName(trackNames) {
     return tracksIdArr
 }
 
-async function createArtistHelper(artists) {
+function createArtistHelper(artists) {
     const artistsIdArr = [];
 
     if (!artists) {
@@ -84,18 +84,15 @@ async function createArtistHelper(artists) {
                     }
                 }
             });
-
-
         }
     }
 
     return artistsIdArr
 }
 
-async function createAlbumHelper(albumTitle, albumYearOfRelease, albumImage) {
-    //Request.body består af et objekt med følgende properties: title STRING, duration INT, artists STRING ARR
+function createAlbumHelper(albumTitle, albumYearOfRelease, albumImage) {
     const values = [albumTitle, albumYearOfRelease, albumImage];
-    const query = "INSERT INTO albums (title, year_of_release, image) VALUES (?,?,?)";
+    const query = "INSERT INTO albums(title, year_of_release, image) VALUES (?,?,?)";
 
     connection.query(query, values, (error, results, fields) => {
         if (error) {
@@ -103,10 +100,79 @@ async function createAlbumHelper(albumTitle, albumYearOfRelease, albumImage) {
             response.status(500).json({ message: "Internal server error" });
         } else {
             const albumID = results.insertId;
+            console.log("HVAD ER ALBUMID ?: " + albumID);
             return albumID
         }
     });
 }
+
+function updateAlbumsArtistsTableHelper(artistsIdArr, albumID) {
+    console.log("Album id her?: " + albumID);
+    const query = `INSERT INTO albums_artists(album_id, artist_id) VALUES (?,?)`;
+    
+    for (const artistID of artistsIdArr) {
+        const values = [albumID, artistID];
+
+        connection.query(query, values, (error, results, fields) => {
+            if (error) {
+                response.status(500).json({ message: "Internal server error" });
+            }
+        })
+    }
+}
+
+function createTrackHelper(track) {
+    const query = `INSERT INTO tracks(title, duration) VALUES (?,?);`
+    const values = [track.title, track.duration];
+
+    connection.query(query, values, (error, results, fields) => {
+        if (error) {
+            response.status(500).json({ message: "Internal server error creating track" });
+        } else {
+            const trackID = results.insertId;
+            return trackID;
+        }
+    })
+}
+
+function CreateTrackInAlbumsTracksHelper(albumID, trackID) {
+    const query = `INSERT INTO albums_tracks(album_id, track_id) VALUES (?,?);`
+    const values = [albumID, trackID];
+    console.log("Album id: " + albumID);
+    console.log("Track id: " + trackID);
+
+    connection.query(query, values, (error, results, fields) => {
+        if (error) {
+            response.status(500).json({ message: "Internal server error CreateTrackInAlbumsTracksHelper" });
+        }
+    })
+}
+
+function CreateTrackInArtistsTracksHelper(artistsIdArr, trackID) {
+    const query = `INSERT INTO artists_tracks(artist_id, track_id) VALUES (?,?);`;
+    for (const artistID of artistsIdArr) {
+        const values = [artistID, trackID];
+
+        connection.query(query, values, (error, results, fields) => {
+            if (error) {
+                console.log(error);
+                response.status(500).json({ message: "Internal server error CreateTrackInAlbumsTracksHelper" });
+            }
+        })
+    }
+}
+
+function populateAlbum(albumID, tracksArr) {
+    for (const track of tracksArr) {
+        //TODO:1 Lav artist først
+        const artistsID = createArtistHelper(track.artists);
+        //TODO:2 Opret track
+        const trackID = createTrackHelper(track);
+        //TODO:3 Opdater albums_tracks OG artists_tracks
+        CreateTrackInAlbumsTracksHelper(albumID, trackID);
+        CreateTrackInArtistsTracksHelper(artistsID, trackID);
+    }
+} 
 
 
 async function createFinishedAlbum(request, response, next) {
@@ -115,22 +181,22 @@ async function createFinishedAlbum(request, response, next) {
     const albumYearOfRelease = albumInformation.albumYearOfRelease;
     const albumImage = albumInformation.albumImage;
     const albumArtistsArr = albumInformation.albumArtists;
-    const songArr = request.body.slice(1);
+    const tracksArr = request.body.slice(1);
 
-    if (!albumInformation || !albumTitle || !albumYearOfRelease || !albumArtistsArr || songArr) {
+    if (!albumInformation || !albumTitle || !albumYearOfRelease || !albumArtistsArr || !tracksArr) {
         response.status(500).json({ message: "Missing info" });
     }
         
-    
-    
     console.log(albumTitle, albumYearOfRelease, albumImage, albumArtistsArr);
-    console.log(songArr);
+    console.log(tracksArr);
 
-    const artistsID = await createArtistHelper(albumArtistsArr);
-    const albumID = await createAlbumHelper(albumTitle, albumYearOfRelease, albumImage, albumArtistsArr)
+    const artistsID = createArtistHelper(albumArtistsArr);
+    const albumID = createAlbumHelper(albumTitle, albumYearOfRelease, albumImage, albumArtistsArr);
+    console.log("Første albumID: " + albumID);
+    updateAlbumsArtistsTableHelper(artistsID, albumID);
+    populateAlbum(albumID, tracksArr);
 
-
-    response.send()
+    response.status(204).json();
 }
 
 export { getAlbumsIDByName, getArtistsIDByName, getTracksIDByName, createArtistHelper, createFinishedAlbum};
