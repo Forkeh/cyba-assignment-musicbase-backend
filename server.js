c623cc1d0777dee232f0d1240179e0ac55763c2e
Chris Skov Sørensen <chso0002@stud.kea.dk>
2023-09-18 20:52:28 +0200

    added comments

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cors from "cors";
-import connection from "./database/dbconfig.js";
 import artistRouter from "./artists/artists.routes.js";
 import albumRouter from "./albums/albums.routes.js";
 import trackRouter from "./tracks/tracks.routes.js";
@@ -8,11 +7,15 @@ import trackRouter from "./tracks/tracks.routes.js";
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Middleware
 app.use(express.json());
 app.use(cors());
 
+// Routes
+app.use("/", trackRouter, albumRouter, artistRouter);
+
+// Start server
 app.listen(port, () => {
     console.log(`Server is running on ${port}`);
 });
 
-app.use("/", trackRouter, albumRouter, artistRouter);
