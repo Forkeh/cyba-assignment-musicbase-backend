45365276999ae5937a3a966b0ba6442b0401d7ff
Chris Skov Sørensen <chso0002@stud.kea.dk>
2023-09-20 22:48:44 +0200

    removed unused imports

--- utils/utils.js
+++ utils/utils.js
@@ -1,8 +1,4 @@
 import connection from "../database/dbconfig.js";
-import {createAlbum} from "../albums/albums.controller.js";
-import {createTrack} from "../tracks/tracks.controllers.js";
-import {createArtist} from "../artists/artists.controller.js";
-
 
 async function getArtistsIDByName(artistNames) {
     const artistIdArr = [];
@@ -12,20 +8,16 @@ async function getArtistsIDByName(artistNames) {
         const values = [artistName];
 
         try {
-
             if (typeof artistName !== "string") {
                 throw new Error("artist is not of type string")
             }
-
 
             const [rows, fields] = await connection.execute(query, values);
             if (rows.length > 0) {
                 artistIdArr.push(rows[0].id);
             }
         } catch (error) {
-            // Handle the error here if needed
             console.error(`Error getting artist ID for ${artistName}: ${error.message}`);
-            // You might want to throw the error or handle it accordingly
             throw error;
         }
     }
@@ -45,9 +37,7 @@ async function getAlbumsIDByName(albumNames) {
                 albumsIdArr.push(rows[0].id);
             }
         } catch (error) {
-            // Handle the error here if needed
             console.error(`Error getting album ID for ${albumName}: ${error.message}`);
-            // You might want to throw the error or handle it accordingly
             throw error;
         }
     }
@@ -94,7 +84,5 @@ async function deleteFromTable(tableName, columnName, id, res) {
         res.status(500).json({ message: "Internal server error" });
     }
 }
-
-
 
 export { getAlbumsIDByName, getArtistsIDByName, searchAll, deleteFromTable };
