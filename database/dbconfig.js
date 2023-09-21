55662689cf4732e6c16e752ddf51f270d960b48f
Forkeh <forkeh@gmail.com>
2023-09-21 11:22:00 +0200

    added certificate check

--- database/dbconfig.js
+++ database/dbconfig.js
@@ -1,12 +1,17 @@
 import mysql from "mysql2/promise";
 import "dotenv/config";
+import fs from "fs/promises";
 
 const connection = await mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
-    multipleStatements: true
+    multipleStatements: true,
 });
+
+if (process.env.DB_CERT) {
+    connection.ssl = { ca: await fs.readFile("DigiCertGlobalRootCA.crt.pem") };
+}
 
 export default connection;
