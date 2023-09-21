81e33269b13e897a69deb20bf00e96b347f639f2
Forkeh <forkeh@gmail.com>
2023-09-21 11:40:47 +0200

    updated dbconfig

--- database/dbconfig.js
+++ database/dbconfig.js
@@ -2,16 +2,18 @@ import mysql from "mysql2/promise";
 import "dotenv/config";
 import fs from "fs/promises";
 
-const connection = await mysql.createConnection({
+const dbConfig = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
     multipleStatements: true,
-});
+};
 
 if (process.env.DB_CERT) {
-    connection.ssl = { ca: await fs.readFile("DigiCertGlobalRootCA.crt.pem") };
+    dbConfig.ssl = { ca: await fs.readFile("DigiCertGlobalRootCA.crt.pem") };
 }
+
+const connection = await mysql.createConnection(dbConfig);
 
 export default connection;
